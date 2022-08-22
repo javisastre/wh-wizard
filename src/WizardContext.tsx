9c1f13c3d90b78cbca44ccd235fd41f17808b024
Javi Sastre <javiersastreg@gmail.com>
2022-08-22 21:48:52 +0200

    updated wizard context to typescript

---
+++ src/WizardContext.tsx
@@ -0,0 +1,44 @@
+import React, {
+  ReactNode,
+  createContext,
+  useState,
+  PropsWithChildren,
+} from "react";
+import { initialWizardData, STEP1 } from "./components/Wizard/utils/constants";
+import {
+  IWizardData,
+  IWizardContextValues,
+} from "./components/Wizard/utils/interfaces";
+
+export const WizardContext = createContext<IWizardContextValues | undefined>(
+  undefined
+);
+
+export const WizardContextProvider = ({
+  children,
+}: PropsWithChildren<ReactNode>) => {
+  const [currentStep, setCurrentStep] = useState<string>(STEP1);
+  const [wizardData, setWizardData] = useState<IWizardData>(initialWizardData);
+  const [step1Done, setStep1Done] = useState<boolean>(false);
+  const [step2Done, setStep2Done] = useState<boolean>(false);
+  const [finished, setFinished] = useState<boolean>(false);
+
+  const wizardContextValues: IWizardContextValues = {
+    currentStep,
+    setCurrentStep,
+    wizardData,
+    setWizardData,
+    step1Done,
+    setStep1Done,
+    step2Done,
+    setStep2Done,
+    finished,
+    setFinished,
+  };
+
+  return (
+    <WizardContext.Provider value={wizardContextValues}>
+      {children}
+    </WizardContext.Provider>
+  );
+};
