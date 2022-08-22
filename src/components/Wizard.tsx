98e7e215084318e6de0032d46c65961159827fa3
Javi Sastre <javiersastreg@gmail.com>
2022-08-22 21:08:34 +0200

    Wizard step router

--- src/components/Wizard.tsx
+++ src/components/Wizard.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { WizardContext } from "../WizardContext";
+
+import Step1 from "./Wizard/steps/Step1";
+import Step2 from "./Wizard/steps/Step2";
+import Step3 from "./Wizard/steps/Step3";
+
+import { STEP1, STEP2, STEP3 } from "./Wizard/utils/constants";
 
 const Wizard = () => {
-  return <div>Wizard</div>;
+  const { currentStep } = useContext(WizardContext);
+
+  return (
+    <div>
+      {currentStep === STEP1 && <Step1 />}
+      {currentStep === STEP2 && <Step2 />}
+      {currentStep === STEP3 && <Step3 />}
+    </div>
+  );
 };
 
 export default Wizard;
