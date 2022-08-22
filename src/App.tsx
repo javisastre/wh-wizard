0109784614b3d4da4e9f91956c47880cb6100e18
Javi Sastre <javiersastreg@gmail.com>
2022-08-22 21:08:44 +0200

    context provider for the wizard

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 
-import WheelHubLogo from "./assets/img/Logotipo-Vertical-Verde-Alta.png";
+// import WheelHubLogo from "./assets/img/Logotipo-Vertical-Verde-Alta.png";
 
 import "./App.scss";
 import Wizard from "./components/Wizard";
+import { WizardContextProvider } from "./WizardContext";
 
 class App extends Component {
   render() {
     return (
       <div className='app'>
-        <Wizard />
+        <WizardContextProvider>
+          <Wizard />
+        </WizardContextProvider>
       </div>
     );
   }
