484d998b7697c49e168c35ffff9547464264b595
Javi Sastre <javiersastreg@gmail.com>
2022-08-27 12:04:56 +0200

    app css reset

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import React, { Component } from "react";
 
 // import WheelHubLogo from "./assets/img/Logotipo-Vertical-Verde-Alta.png";
 
-import "./App.scss";
 import Wizard from "./components/Wizard/Wizard";
 import { WizardContextProvider } from "./WizardContext";
 
 class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div>
         <WizardContextProvider>
           <Wizard />
         </WizardContextProvider>
