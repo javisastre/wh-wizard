aa7d35adbd6e3b47a1bc2234ac4d73ad7c87b3d0
Javi Sastre <javiersastreg@gmail.com>
2022-08-27 18:01:16 +0200

    text area basic component

---
+++ src/components/Wizard/inputs/TextareaInput.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+import { IInput, TInputsEvent } from "../utils/interfaces";
+
+const TextareaInput = ({
+  name,
+  label,
+  placeholder,
+  value,
+  required,
+  min,
+  max,
+  handleInput,
+}: IInput) => {
+  const [count, setCount] = useState<number>(0);
+
+  const handleChanges = (e: TInputsEvent) => {
+    handleInput(e);
+    setCount(e.target.value.length);
+  };
+
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <textarea
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        minLength={min}
+        maxLength={max}
+        required={required}
+        value={value}
+        onChange={handleChanges}
+      ></textarea>
+      <div>
+        {count}/{max}
+      </div>
+    </div>
+  );
+};
+
+export default TextareaInput;
