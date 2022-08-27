ca7f8b0eea125e75e3e3388be65ee281872cd6ab
Javi Sastre <javiersastreg@gmail.com>
2022-08-27 14:19:33 +0200

    added validators and error messages

--- src/components/Wizard/utils/constants.ts
+++ src/components/Wizard/utils/constants.ts
@@ -1,4 +1,4 @@
-import { IStep1, IStep2, IWizardData } from "./interfaces";
+import { IStep1, IStep2, IValidators, IWizardData } from "./interfaces";
 
 export const STEP1 = "step1";
 export const STEP2 = "step2";
@@ -15,7 +15,28 @@ export const initialStep2: IStep2 = {
   hint: "",
 };
 
+export const initialStep2Error: IStep2 = {
+  username: "",
+  password: "",
+  repeatPassword: "",
+  hint: "",
+};
+
 export const initialWizardData: IWizardData = {
   step1: initialStep1,
   step2: initialStep2,
+};
+
+export const validators: IValidators = {
+  username: /[0-9a-zA-Z]{3,}/,
+  password: /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,24}$/,
+  repeatPassword: /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,24}$/,
+  hint: /^.{0,60}$/,
+};
+
+export const errorMessages: IStep2 = {
+  username: "Debes introducir un usuario válido",
+  password: "Tu password no es suficientemente compleja",
+  repeatPassword: "Debe ser igual que la anterior",
+  hint: "No debe ser mayor de 60 caracteres",
 };
