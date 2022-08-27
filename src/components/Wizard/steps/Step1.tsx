41316bde27655decb54e8adb3322c62fd700fe51
Javi Sastre <javiersastreg@gmail.com>
2022-08-27 12:42:13 +0200

    step 1 implemented properly

--- src/components/Wizard/steps/Step1.tsx
+++ src/components/Wizard/steps/Step1.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { ChangeEvent, useContext, useEffect, useState } from "react";
+import { WizardContext } from "../../../WizardContext";
+import { IStep1, IWizardData } from "../utils/interfaces";
 
 const Step1 = () => {
-  return <div>Step1</div>;
+  const { setStep1Done, wizardData, setWizardData } = useContext(WizardContext);
+
+  const [data, setData] = useState<IStep1>(wizardData.step1);
+
+  const handleData = (e: ChangeEvent<HTMLInputElement>) =>
+    setData({ ...data, confirmPrivacy: e.target.checked });
+
+  useEffect(() => {
+    const isAllDataFilled = () => (data.confirmPrivacy ? true : false);
+
+    isAllDataFilled() && setStep1Done(isAllDataFilled());
+    setWizardData((prev: IWizardData) => ({ ...prev, step1: data }));
+  }, [data, setStep1Done, setWizardData]);
+
+  return (
+    <div>
+      <h2>step1</h2>
+      <div>
+        <h6>¿Qué deberá realizar?</h6>
+        <p>
+          En la primera prestaña, deberá confirmar que es mayor de edad y acepta
+          el tratamiento de sus datos según la política de datos vigentes.
+        </p>
+        <p>
+          En la segunda pestaña, deberá crear un usuario, una contraseña y una
+          pista para recordar la contraseña (como dato opcional).
+        </p>
+        <p>
+          En tercer lugar, deberá visualizarse el mensaje de éxito de creación.
+        </p>
+      </div>
+      <input
+        id='confirmPrivacy'
+        name='confirmPrivacy'
+        type='checkbox'
+        checked={data.confirmPrivacy}
+        onChange={handleData}
+      />
+      <label htmlFor='confirmPrivacy'>
+        Confirma que es mayor de edad, y acepta el tratamiento de sus datos
+        según la política de protección de datos vigente.
+      </label>
+    </div>
+  );
 };
 
 export default Step1;
