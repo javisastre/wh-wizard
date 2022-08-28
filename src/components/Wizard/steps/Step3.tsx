361407dc057c97dc4c2e6ed0e9e1a52ec1f22707
Javi Sastre <javiersastreg@gmail.com>
2022-08-28 16:27:23 +0200

    initial content on step3

--- src/components/Wizard/steps/Step3.tsx
+++ src/components/Wizard/steps/Step3.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Step3 = () => {
-  return <div>Step3</div>;
+  return (
+    <div className='step3'>
+      <div>
+        <div>
+          <img src='' alt='' />
+        </div>
+        <h2>¡La cuenta se creó correctamente!</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. At in
+          tellus integer feugiat scelerisque varius. Dignissim cras tincidunt
+          lobortis feugiat vivamus at. Id neque aliquam vestibulum morbi blandit
+          cursus. Nulla pharetra diam sit amet nisl suscipit adipiscing.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Step3;
