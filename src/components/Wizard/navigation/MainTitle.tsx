cce211a8a24045b3d4d2d96185c459453b7d82aa
Javi Sastre <javiersastreg@gmail.com>
2022-08-28 17:43:11 +0200

    easter egg

--- src/components/Wizard/navigation/MainTitle.tsx
+++ src/components/Wizard/navigation/MainTitle.tsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const MainTitle = () => (
   <h1 className='main-title'>
-    <a href='#'>Test</a> Frontend Wheel Hub
+    <a
+      href='https://github.com/javisastre'
+      target='_blank'
+      rel='noopener noreferrer'
+    >
+      Test
+    </a>{" "}
+    Frontend Wheel Hub
   </h1>
 );
 
