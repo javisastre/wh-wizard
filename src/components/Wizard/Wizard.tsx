2a267b7c943f4122d2762421fa2e4e3ad0ba1fa3
Javi Sastre <javiersastreg@gmail.com>
2022-08-28 11:13:15 +0200

    implemented hook to switch between steps

--- src/components/Wizard/Wizard.tsx
+++ src/components/Wizard/Wizard.tsx
@@ -1,26 +1,19 @@
 import React from "react";
 
-import { useWizardContext } from "./context/WizardContext";
+import useStepSelector from "./hooks/useStepSelector";
 import NavButtons from "./navigation/NavButtons";
 import NavSteps from "./navigation/NavSteps";
-import Step1 from "./steps/Step1";
-import Step2 from "./steps/Step2";
-import Step3 from "./steps/Step3";
-
-import { STEP1, STEP2, STEP3 } from "./utils/constants";
 
 import "./Wizard.scss";
 
 const Wizard = () => {
-  const { currentStep } = useWizardContext();
+  const StepComponent = useStepSelector();
 
   return (
     <div>
       <NavSteps />
       <main>
-        {currentStep === STEP1 && <Step1 />}
-        {currentStep === STEP2 && <Step2 />}
-        {currentStep === STEP3 && <Step3 />}
+        <StepComponent />
       </main>
       <NavButtons />
     </div>
