93c2ae9135050fd3757413e4659ad42d84ca000e
Javi Sastre <javiersastreg@gmail.com>
2022-08-29 18:53:16 +0200

    404 translation

--- src/components/Wizard/steps/Step404.tsx
+++ src/components/Wizard/steps/Step404.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const Step404 = () => {
+  const { t } = useTranslation();
   return (
     <div>
       <h1>Error 404</h1>
-      <p>There has been an internal error</p>
+      <p>{t("STEP404")}</p>
     </div>
   );
 };
