3d4d7c086499f6d0e61949e4a502bbb99a45e6a6
Javi Sastre <javiersastreg@gmail.com>
2022-08-31 12:25:40 +0200

    textinput testing suite

--- src/__tests__/TextInput.spec.tsx
+++ src/__tests__/TextInput.spec.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import TextInput from "./../components/Wizard/steps/Step2/TextInput";
 import { IInput } from "../components/Wizard/utils/interfaces";
+
+const mockHandleInput = jest.fn();
 
 const props: IInput = {
   name: "name",
@@ -12,13 +15,21 @@ const props: IInput = {
   min: 3,
   value: "",
   error: "",
-  handleInput: jest.fn(),
+  handleInput: mockHandleInput,
   handleError: jest.fn(),
 };
 
 describe("TextInput component test suite", () => {
   test("should render the component with props", () => {
     render(<TextInput {...props} />);
-    screen.logTestingPlaygroundURL();
+    const input = screen.getByRole("textbox", { name: /your name/i });
+    expect(input).toBeInTheDocument();
+  });
+  test("should render the component and call mockHandleInput", () => {
+    render(<TextInput {...props} />);
+    const input = screen.getByRole("textbox", { name: /your name/i });
+    userEvent.clear(input);
+    userEvent.type(input, "judith butler");
+    expect(mockHandleInput).toHaveBeenCalled();
   });
 });
